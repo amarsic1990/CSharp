79ea8718099737acc66431b36392a0b9dbca0143
amarsic1990 <marsic.ante@hotmail.com>
2017-01-22 20:02:02 +0100

    First commit

---
+++ KarteLibrary/KarteLibrary/Card.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KarteLibrary
+{
+    public class Card
+    {
+        public readonly Rank rank;
+        public readonly Suit suit;
+
+        private Card() { }
+
+        public Card(Suit newSuit, Rank newRank)
+        {
+            suit = newSuit;
+            rank = newRank;
+        }
+
+        public override string ToString() => "The " + rank + " of " + suit + "s"; 
+    }
+}
