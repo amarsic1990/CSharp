bcd35b6808a8e9e9f5ab48982c311916dd8380b8
amarsic1990 <marsic.ante@hotmail.com>
2017-01-28 19:02:19 +0100

    First commit

---
+++ KarteLibraryCollectionConsoleApp/KarteLibraryCollectionConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using KarteLibraryCollection;
+
+namespace KarteLibraryCollectionConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck igrajuciDek = new Deck();
+
+            //for (int i = 0; i < 52; i++)
+            //{
+            //    Console.WriteLine(igrajuciDek.VratiKartu(i));
+            //}
+
+            //Console.ReadKey();
+
+            igrajuciDek.IzmjesajKarte();
+
+            for (int i = 0; i < 52; i++)
+            {
+                Console.WriteLine(igrajuciDek.VratiKartu(i));
+            }
+            Console.ReadKey();
+        }
+    }
+}
