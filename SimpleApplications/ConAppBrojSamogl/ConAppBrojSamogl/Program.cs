7dd38f2c90b92468bda310ecb54007a7438b16ee
Ante Maršić <amarsic@hattrick.local>
2019-12-22 13:58:12 +0100

    sorted...

---
+++ SimpleApplications/ConAppBrojSamogl/ConAppBrojSamogl/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppBrojSamogl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] samoglasnici = { 'a', 'e', 'i', 'o', 'u' };
+            Console.WriteLine("Molim unesite riječ: ");
+            string ime = Console.ReadLine();
+            int brojSamoglasnika = 0;
+
+            for (int i = 0; i < samoglasnici.Length; i++)
+            {
+                for (int j = 0; j < ime.Length; j++)
+                {
+                    if (ime[j] == samoglasnici[i])
+                    {
+                        brojSamoglasnika += 1;
+                    }
+                }
+            }
+            Console.WriteLine($"broj samoglasnika u rijeci: { brojSamoglasnika}");
+            Console.ReadKey();
+        }
+    }
+}
