e5613ac925017806f1d804fea9843b20fafcb874
Ante Maršić <amarsic@hattrick.local>
2019-12-22 11:26:00 +0100

    EF folder added

---
+++ EntityFramework/DBFirstVidzy/DBFirstVidzy/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBFirstVidzy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var context = new VidzyDbContext();
+            //context.AddVideo("Parada", DateTime.Now, "Comedy");
+            //context.AddVideo("Maršal", DateTime.Now, "Comedy");
+            //context.AddVideo("Kako je poceo rat na mom otoku", DateTime.Now, "Action");
+
+            //context.AddVideo("žikina dinastija", DateTime.Now, 1);
+
+
+            context.AddVideo("Kad mrtvi zapjevaju", DateTime.Now, 1,2);
+        }
+    }
+}
